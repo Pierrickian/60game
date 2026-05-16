0b39eaa218359477f39276f18a427eb377a84156
Pierrickian <8685110+Pierrickian@users.noreply.github.com>
2026-05-17 01:03:22 +0200

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true'
+
 export default defineConfig({
+  base: isGitHubPages ? '/60game/' : '/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
